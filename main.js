88938ee49409d104603b1b5019e01cd8221ad4b3
PeterTV <challengepeter41@gmail.com>
2022-03-29 19:59:59 +0200

    ajout du dt

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-let canvas = document.getElementById("my_canvas");
+let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 let interval;
 
+let dernierUpdate = Date.now();
+
 function run() {
+    let maintenant = Date.now();
+    let dt = (maintenant - dernierUpdate) / 1000;
+    dernierUpdate = maintenant;
    // console.log("run !");
-    update();
+    update(dt);
     ctx.clearRect(0,0, canvas.width, canvas.height);
     draw(ctx);
 }
